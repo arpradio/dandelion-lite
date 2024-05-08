77558b90c4657322eee8f8c09cdd750aa3de70f0
zxpectre <15870846+zxpectre@users.noreply.github.com>
2024-05-08 00:57:30 -0300

    fix: duplicated lines commented out and files re-prioritized on koios-artifacts

--- scripts/sql/rpc/20_koios-artifacts/___db-scripts/00_reset_grest.sql
+++ scripts/sql/rpc/20_koios-artifacts/___db-scripts/00_reset_grest.sql
@@ -1,11 +1,11 @@
 -- Recreate grest schema
 DROP SCHEMA IF EXISTS grest CASCADE;
 
-CREATE SCHEMA grest;
+-- CREATE SCHEMA grest;
 
-GRANT USAGE ON SCHEMA grest TO authenticator, web_anon;
+-- GRANT USAGE ON SCHEMA grest TO authenticator, web_anon;
 
-GRANT SELECT ON ALL TABLES IN SCHEMA grest TO authenticator, web_anon;
+-- GRANT SELECT ON ALL TABLES IN SCHEMA grest TO authenticator, web_anon;
 
-ALTER DEFAULT PRIVILEGES IN SCHEMA grest GRANT
-SELECT ON TABLES TO authenticator, web_anon;
+-- ALTER DEFAULT PRIVILEGES IN SCHEMA grest GRANT
+-- SELECT ON TABLES TO authenticator, web_anon;
