200ec0f1d773ad5931560873d1c6b32f08ec9bd2
zxpectre <15870846+zxpectre@users.noreply.github.com>
2024-05-07 21:51:50 -0300

    fix: grest schema hardcoded on source files

--- scripts/sql/rpc/10_koios-lite/03_cached_tables/asset_info_cache.sql
+++ scripts/sql/rpc/10_koios-lite/03_cached_tables/asset_info_cache.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS {{SCHEMA}}.asset_info_cache (
 
 CREATE INDEX IF NOT EXISTS idx_first_mint_tx_id ON {{SCHEMA}}.asset_info_cache (first_mint_tx_id);
 CREATE INDEX IF NOT EXISTS idx_last_mint_tx_id ON {{SCHEMA}}.asset_info_cache (last_mint_tx_id);
-CREATE INDEX IF NOT EXISTS idx_creation_time ON grest.asset_info_cache (creation_time DESC);
+CREATE INDEX IF NOT EXISTS idx_creation_time ON {{SCHEMA}}.asset_info_cache (creation_time DESC);
 
 CREATE OR REPLACE FUNCTION {{SCHEMA}}.asset_info_cache_update()
 RETURNS void
