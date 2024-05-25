f43b650783c9e67229cb7519799d7891cb68dad7
zxpectre <15870846+zxpectre@users.noreply.github.com>
2024-05-24 22:24:54 -0300

    feat: security and access improvements
    - forced a read-only mode for dbless-cardano-token-registry
    - removed local image name from dbless-cardano-token-registry and postgres on docker-compose.yaml to avoid unwanted pulls
    - pointed the gundb static server to an empty www dir by default on unimatrix
    - made a health path for unimatrix to be used by haproxy
    - added CORS headers by default to all responses on haproxy to support dandelion clients
    - added some haproxy timeouts suggested for websockets (unimatrix, ogmios, etc..)
    - other minor stuff

--- src/unimatrix/unimatrix.js
+++ src/unimatrix/unimatrix.js
@@ -1,3 +1,4 @@
+// Notes: https://github.com/rozek/notes-on-gundb
 ;(function(){
 	var cluster = require('cluster');
 	if(cluster.isMaster){
@@ -9,19 +10,21 @@
 	}
 
 	var fs = require('fs');
+	var path = require('path');
 	var config = {
 		port: process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765,
 		peers: process.env.PEERS && process.env.PEERS.split(',') || [],
 	};
-	const wwwDir=process.env.WWW || "";//__dirname
-	var Gun = require('gun');  
+	const wwwDir=process.env.WWW || path.join(__dirname, 'www');
+	var Gun = require('gun');	
+	const staticMiddleware=Gun.serve(wwwDir);	
 	if(process.env.HTTPS_KEY){
 		config.key = fs.readFileSync(process.env.HTTPS_KEY);
 		config.cert = fs.readFileSync(process.env.HTTPS_CERT);
-		config.server = require('https').createServer(config, Gun.serve(wwwDir));
+		config.server = require('https').createServer(config, staticMiddleware);
     		console.info(`[UNIMATRIX] using SSL`);
 	} else {
-		config.server = require('http').createServer(Gun.serve(wwwDir));
+		config.server = require('http').createServer(staticMiddleware);
 	}
 	var cfg = {
     		debug:process.env.DEBUG,
@@ -34,6 +37,7 @@
 	if(cfg.wwwDir){
     		console.info(`[UNIMATRIX] serving ${cfg.wwwDir}`);
   	}
+	
 	var gun = Gun({web: config.server.listen(cfg.port), peers: cfg.peers});
 
 	console.info(`[UNIMATRIX] Relay peer started on port ${cfg.port} with ${cfg.gunRoute}`);
