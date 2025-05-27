f67e4109e2ad70034196daef2616fb823b2020e6
zxpectre <15870846+zxpectre@users.noreply.github.com>
2025-05-27 04:46:26 +0000

    feat: removed some of koios because it's not compatible with blockfrost's dbsync requirements

--- scripts/sql/rpc/20_koios-artifacts_1.2.0a_[SKIP]/03_utilities/cli_protocol_params.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION grest.cli_protocol_params()
-RETURNS JSON
-LANGUAGE sql STABLE
-AS $$
-  SELECT ct.artifacts::json
-  FROM grest.control_table AS ct
-  WHERE ct.key = 'cli_protocol_params';
-$$;
